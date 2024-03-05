2211137b193d445429c54934ac41c797561d0f6b
Vinicius Santiago <vinicius.santiago@sap.com>
2024-03-05 20:32:45 -0300

    pytest

--- tests/test_routine.py
+++ tests/test_routine.py
@@ -1,4 +1,23 @@
-from routine import Activity
+from src.routine import Activity, Activities, frequency_choice
 
-def test_freq_custom():
-    activity = Activity()
+# AAA - 3A -> Arrange Act Assert
+def test_constructor_activity():
+    activity = Activity("leitura", "7")
+    result = activity.freq
+    assert result == '7'
+
+def test_return_frequency_choice_function():
+    result_name = frequency_choice("weekend")
+    result_number = frequency_choice("2")
+    assert result_name == '2' and result_number == '2'
+
+def test_add_activity():
+    # TODO: improve
+    name_activity = "leitura"
+    manage_activities = Activities()
+    manage_activities.add_activity(name_activity, "5")
+    for activity in manage_activities.activities:
+        if activity.name == name_activity:
+            result = True
+    assert result == True
+        
